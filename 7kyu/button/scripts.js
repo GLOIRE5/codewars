f0eea983867634bd2bf1c27c9c86c4a7304c5569
GLOIRE GWIZA <g.gwiza@alustudent.com>
2025-05-24 15:54:03 +0300

    done

---
+++ 7kyu/button/scripts.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('colorInput');
+const defaultColor = 'white';
+
+function isValidColor(color) {
+  const test = document.createElement('div');
+  test.style.color = color;
+  return test.style.color !== '';
+
+}
+
+input.addEventListener('input', () => {
+  const color = input.value.trim();
+  if (isValidColor(color)) {
+    document.body.style.backgroundColor = color;
+  }
+});
+
+resetBtn.addEventListener( 'click', () => {
+  document.body.style.backgroundColor = defaultColor;
+  input.value = '';
+});
